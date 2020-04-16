d3461e6cb6b268937b3e46263d393f7a3c2a8334
aolland <alexolland@getcodingknowledge.com>
2020-04-16 16:10:09 +0200

    2-3

---
+++ Section 2/2-3/main.js
@@ -0,0 +1,14 @@
+const { Client } = require('discord.js');
+const { TOKEN, PREFIX } = require('./config');
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  if (message.content.startsWith(`${PREFIX}salut`)) message.channel.send("Je suis le bot!")
+  if (message.content.startsWith(`${PREFIX}ping`)) message.channel.send("Pong!")
+});
+
+client.login(TOKEN);
