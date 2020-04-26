801c6873adee3ba64d321f06d8c1a0d17d8a2087
aolland <alexolland@getcodingknowledge.com>
2020-04-26 16:20:06 +0200

    3-7/3-8

---
+++ Section 3/3-7/commands/Misc/ping.js
@@ -0,0 +1,10 @@
+module.exports.run = (client, message, args) => {
+  message.channel.send("Pong!");
+};
+
+module.exports.help = {
+  name: "ping",
+  description: "Renvoie pong!",
+  cooldown: 10,
+  args: false
+};
