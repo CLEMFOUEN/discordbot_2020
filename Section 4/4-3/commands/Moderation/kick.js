b9604b4ea556b6c2271a93ff6462b7ff5449a843
aolland <alexolland@getcodingknowledge.com>
2020-04-29 16:15:13 +0200

    Correction 4-3

--- Section 4/4-3/commands/Moderation/kick.js
+++ Section 4/4-3/commands/Moderation/kick.js
@@ -1,9 +1,9 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports.run = (client, message, args) => {
-  const user = message.mentions.users.first();
-  const reason = (args.splice(1).join(' ') || 'Aucune raison spécifiée');
-  user ? message.guild.member(user).kick(reason) : message.channel.send("L'utilisateur n'existe pas!");
+  let user = message.mentions.users.first();
+  let reason = (args.splice(1).join(' ') || 'Aucune raison spécifiée');
+  user ? message.guild.member(user).kick(reason) : message.channel.send("L'utilisateur n'existe pas.");
 
   const embed = new MessageEmbed()
     .setAuthor(`${user.username} (${user.id})`)
@@ -12,9 +12,8 @@ module.exports.run = (client, message, args) => {
     .setThumbnail(user.avatarURL())
     .setTimestamp()
     .setFooter(message.author.username, message.author.avatarURL());
-
-  const log_channel = client.channels.cache.get('705055460255989841');
-  log_channel.send(embed)
+    
+  client.channels.cache.get('705055460255989841').send(embed);
 };
 
 module.exports.help = {
@@ -22,7 +21,7 @@ module.exports.help = {
   aliases: ['kick'],
   description: "Kick un utilisateur",
   cooldown: 10,
-  usage: '<votre_message>',
+  usage: '<user> <raison>',
   isUserAdmin: true,
   permissions: true,
   args: true
